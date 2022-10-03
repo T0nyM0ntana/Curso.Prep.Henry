a49feb3f8e9d9b376e5e763ddf2d36b10175bf4c
TTONYMONTANA <goltin_hailaf@hotmail.com>
2022-10-03 17:10:31 -0500

    finish homework 1

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -2,8 +2,7 @@
 
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un  array (pasado por parametro)
-  // Tu código:
-}
+  
 
 
 function devolverUltimoElemento(array) {
